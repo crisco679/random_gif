0fe02f6da5dde1772cb098f058ed1aad49768241
christuttle <crunch.tuttle@gmail.com>
2016-04-01 12:57:38 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+$(function(){
+
+
+  $("button").on("click",function(){
+    $.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC').done(function(response){
+
+      var randomGif = response.data;
+      console.log(response);
+      console.log(randomGif.image_url);
+      $(".gif").append("<img src =" + randomGif.image_url + ">");
+    });
+  });
+
+
+
+
+});
